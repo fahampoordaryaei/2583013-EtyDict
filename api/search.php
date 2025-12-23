fbaff329b4ac17cc88877b5de61c7db12dc95ab5
fahampoordaryaei <fahrmpo@proton.me>
2025-12-23 01:34:26 +0100

    Debug search.php json only

--- api/search.php
+++ api/search.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../src/api/dict.php';
-require_once __DIR__ . '/../src/api/ety.php';
+// require_once __DIR__ . '/../src/api/dict.php';
+// require_once __DIR__ . '/../src/api/ety.php';
 require_once __DIR__ . '/../src/api/json.php';
-require_once __DIR__ . '/../src/log/eventlogger.php';
+// require_once __DIR__ . '/../src/log/eventlogger.php';
 
-echo "DEBUG SEARCH AFTER REQUIRE"; exit;
+echo "DEBUG SEARCH JSON ONLY"; exit;
 
 function searchApiHandler(): void
 require_once __DIR__ . '/../src/api/ety.php';
